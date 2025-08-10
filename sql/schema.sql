307db418931ea59ebb3f02f50333d62a890cb457
changethislaterforsure <benjmain.legge@keyin.comm>
2025-08-10 14:03:03 -0230

    Updated schema and tested logger

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,14 @@
--- Users Table
+
+
+BEGIN;
+
+
+DROP TABLE IF EXISTS memberships CASCADE;
+DROP TABLE IF EXISTS workout_classes CASCADE;
+DROP TABLE IF EXISTS merchandise CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+-- ===== USERS =====
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -9,28 +19,32 @@ CREATE TABLE users (
     role VARCHAR(20) NOT NULL
 );
 
--- Memberships Table
+-- ===== MEMBERSHIPS =====
 CREATE TABLE memberships (
     membership_id SERIAL PRIMARY KEY,
     membership_type VARCHAR(50) NOT NULL,
-    membership_description TEXT,
-    membership_cost DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    cost DECIMAL(10, 2) NOT NULL,
     member_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
 
--- Workout Classes Table
+-- ===== WORKOUT CLASSES =====
 CREATE TABLE workout_classes (
-    workout_class_id SERIAL PRIMARY KEY,
-    class_type VARCHAR(50) NOT NULL,
-    class_description TEXT,
+    class_id SERIAL PRIMARY KEY,
+    class_name VARCHAR(50) NOT NULL,
+    description TEXT,
+    date DATE,
+    time TIME,
     trainer_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL
 );
 
--- Gym Merchandise Table
-CREATE TABLE gym_merch (
-    merch_id SERIAL PRIMARY KEY,
-    merch_name VARCHAR(100) NOT NULL,
-    merch_type VARCHAR(50),
-    merch_price DECIMAL(10, 2) NOT NULL,
-    quantity_in_stock INTEGER NOT NULL
+-- ===== MERCHANDISE =====
+CREATE TABLE merchandise (
+    item_id SERIAL PRIMARY KEY,
+    item_name VARCHAR(100) NOT NULL,
+    description VARCHAR(100),
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INTEGER NOT NULL
 );
+
+COMMIT;

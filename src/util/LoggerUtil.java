f1cc675c416375c3381bd0ecf80db0ef660cb576
changethislaterforsure <benjmain.legge@keyin.comm>
2025-08-10 12:13:45 -0230

    Created Logger

---
+++ src/util/LoggerUtil.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class LoggerUtil {
+    private static final Logger logger = Logger.getLogger(LoggerUtil.class.getName());
+
+    static {
+        try {
+            LogManager.getLogManager().reset();
+            FileHandler fh = new FileHandler("resources/logger.txt", true); // ✅ now writes to the correct file
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+            logger.setLevel(Level.INFO);
+        } catch (IOException e) {
+            System.err.println("Could not set up logger: " + e.getMessage());
+        }
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
